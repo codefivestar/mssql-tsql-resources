4f76426ad42ac423da2d6097f99acd08d267a4e8
Hidequel Puga <50523807+codefivestar@users.noreply.github.com>
2023-06-26 14:09:25 -0500

    Update Mirror Configuration.sql
    Ordena comandos

--- Mirror Configuration.sql
+++ Mirror Configuration.sql
@@ -1,10 +1,7 @@
 ----------------------------------------------------------------------------------------------------------
---File Name   : MSSQL – Mirror Configuration.sql
---Create Date : 2018-07-23 10:07 A.M.
---Author      : Hidequel Puga
---Mail        : bounty31k@outlook.com
---Reference   : https://docs.microsoft.com/en-us/sql/database-engine/database-mirroring/database-mirroring-sql-server?view=sql-server-2017
---Description : MS SQL Server Mirror Configuration with out Witness Instance.
+-- Author      : Hidequel Puga
+-- Description : MS SQL Server Mirror Configuration with out Witness Instance.
+-- Reference   : https://docs.microsoft.com/en-us/sql/database-engine/database-mirroring/database-mirroring-sql-server?view=sql-server-2017
 ----------------------------------------------------------------------------------------------------------
 
 --Server Instance / Database:
@@ -13,13 +10,14 @@
 --Witness   --> No Configurado
 --Database  --> D5S_test
 
---Paso # 1 : Realizar un backup full y del transaction log en un folder compartido.
---           El backup se debe realizar desde el servidor Principal.
+--Paso # 1 : Aplicar en Instancia Principal
 
 BACKUP DATABASE D5S_test TO DISK = '\\SharedFolder_temp\D5S_test.bak' WITH FORMAT;
 BACKUP LOG D5S_test TO DISK      = '\\SharedFolder_temp\D5S_test.trn';
 
---Paso # 2 : Restaurar el backup full y transaction log, en el servidor Mirror.
+----------------------------------------------------------------------------------------------------
+
+--Paso # 2 : Aplicar en Instancia Mirror
 
 IF DB_ID('D5S_test') > 0 DROP DATABASE D5S_test;
 
@@ -30,13 +28,21 @@ MOVE 'D5S_test_log' TO 'E:\MSSQL\DATA\D5S_test_log.ldf';
 
 RESTORE LOG D5S_test FROM DISK = '\\SharedFolder_temp\D5S_test.trn' WITH NORECOVERY, FILE = 1;
 
---Paso # 3 : Aplicar SET PARTNER desde la instancia Principal
+----------------------------------------------------------------------------------------------------
+
+--Paso # 3 : Aplicar en Instancia Mirror
 ALTER DATABASE D5S_test SET PARTNER = 'TCP://SSInstance_1.DOMAIN:5022';
 
---Paso # 4 : Aplicar SET PARTNER desde la instancia Mirror
+----------------------------------------------------------------------------------------------------
+
+--Paso # 4 : Aplicar en Instancia Principal
 ALTER DATABASE D5S_test SET PARTNER = 'TCP://SSInstance_2.DOMAIN:5022';
 
+----------------------------------------------------------------------------------------------------
 
-----Comando para Eliminar el Mirror entre base de datos.
-ALTER DATABASE D5S_test SET PARTNER OFF  
-RESTORE DATABASE D5S_test WITH RECOVERY; 
+--Paso # 5 : Aplicar en Instancia Mirror (Si fuera necesario)
+
+--Comando para Eliminar el Mirror entre base de datos.
+
+-- ALTER DATABASE D5S_test SET PARTNER OFF  
+-- RESTORE DATABASE D5S_test WITH RECOVERY; 
