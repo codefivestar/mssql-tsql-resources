b7ce31436fc655bd2c754da5b968d795f64c97e5
Hidequel Puga <50523807+codefivestar@users.noreply.github.com>
2023-08-10 09:27:26 -0500

    Update Databases Size.sql
    routine is updated to retrieve database sizes

--- Databases Size.sql
+++ Databases Size.sql
@@ -1,35 +1,50 @@
---  SELECT sys.databases.database_id
---       --, sys.databases.name AS DBName
---       , CONVERT(VARCHAR,SUM(size) * 8 / 1024) + ' MB' AS TotalDiskSpace
---    FROM sys.databases   
---    JOIN sys.master_files  
---      ON sys.databases.database_id = sys.master_files.database_id  
---GROUP BY sys.databases.database_id;
+--=========================================================================================================
+-- Author       : Hidequel Puga
+-- Date         : 2023-08-10
+-- Description  : Database Sizes
+-- Requirements : #DBA #DBAWorks #DBARules
+--=========================================================================================================
 
---exec sp_spaceused  
+BEGIN -- ## Option 1 ## ==================================================================================================
 
---
+	  SELECT DB_NAME(database_id) AS 'Database'
+           , SUM(size * 8 / 1024) AS 'Size (MB)'
+        FROM sys.master_files
+       WHERE type_desc IN ('ROWS', 'LOG')
+    GROUP BY database_id;
+	
+END
 
-USE [master]
-GO
+BEGIN -- ## Option 2 ## ==================================================================================================
 
-SELECT @@SERVERNAME           AS [Server Name]
-     , T1.[name]              AS [DB Name]
-	 , ''                     AS [Db Apps]
-     , T1.state_desc          AS [State]
-	 , T1.recovery_model_desc AS [Recovery Model]
-	 , T1.compatibility_level AS [Compatibility Level]
-	 , T1.collation_name      AS [Collation Name]
-	 , T2.[Total Disk Space]
-  FROM sys.databases AS T1
-  JOIN (
-		SELECT sys.databases.database_id
-		   --, sys.databases.name AS DBName
-		     , CONVERT(VARCHAR,SUM(size) * 8 / 1024) + ' MB' AS [Total Disk Space]
-		  FROM sys.databases   
-		  JOIN sys.master_files  
-		    ON sys.databases.database_id = sys.master_files.database_id  
-	  GROUP BY sys.databases.database_id
-        ) AS T2
-	ON T1.database_id = T2.database_id
-	;  
+	SELECT @@SERVERNAME           AS [Server Name]
+		 , T1.[name]              AS [DB Name]
+		 , ''                     AS [Db Apps]
+		 , T1.state_desc          AS [State]
+		 , T1.recovery_model_desc AS [Recovery Model]
+		 , T1.compatibility_level AS [Compatibility Level]
+		 , T1.collation_name      AS [Collation Name]
+		 , T2.[Total Disk Space]
+	  FROM sys.databases AS T1
+	  JOIN (
+			SELECT sys.databases.database_id
+			   --, sys.databases.name AS DBName
+				 , CONVERT(VARCHAR,SUM(size) * 8 / 1024) + ' MB' AS [Total Disk Space]
+			  FROM sys.databases   
+			  JOIN sys.master_files  
+				ON sys.databases.database_id = sys.master_files.database_id  
+		  GROUP BY sys.databases.database_id
+			) AS T2
+		ON T1.database_id = T2.database_id
+	ORDER BY T2.[Total Disk Space]; 
+
+END
+
+BEGIN -- ## Option 3 ## ==================================================================================================
+	
+	EXEC sp_spaceused
+
+END
+
+
+
